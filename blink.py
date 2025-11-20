d8e54b3513aaf285481c55b4e94dc5071e9a5b62
tghju <isaac.2008.xiu@gmail.com>
2025-11-20 10:57:55 -0500

    Blinking Twice
    When button pressed, LED blinks twice

--- blink.py
+++ blink.py
@@ -7,6 +7,11 @@ button = Pin(14, Pin.IN, Pin.PULL_UP)
 
 while True:
     if button.value() == 0:
-        led.value(1)
+        for i in range(0, 2):
+            led.value(1)
+            time.sleep(0.1)
+            led.value(0)
+            time.sleep(0.1)
     else:
         led.value(0)
+    time.sleep(0.2)
