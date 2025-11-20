cc1e08c628c72b10f115f83d450ce85b06f142d7
tghju <isaac.2008.xiu@gmail.com>
2025-11-20 11:06:51 -0500

    Single Burst Press
    Should only blink twice until the button is released and pressed again.

--- blink.py
+++ blink.py
@@ -5,13 +5,17 @@ led = Pin(15, Pin.OUT)
 
 button = Pin(14, Pin.IN, Pin.PULL_UP)
 
+can_blink = True
+
 while True:
-    if button.value() == 0:
+    if button.value() == 0 and can_blink == True:
         for i in range(0, 2):
             led.value(1)
             time.sleep(0.1)
             led.value(0)
             time.sleep(0.1)
+        can_blink = False
     else:
         led.value(0)
-    time.sleep(0.2)
+        if button.value() == 1 and can_blink == False:
+            can_blink = True
